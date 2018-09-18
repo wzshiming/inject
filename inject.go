24b6bad221d547995a88045e50b0d88a2e243648
wzshiming <wzshiming@foxmail.com>
2018-09-18 15:34:16 +0800

    first commit

---
+++ inject.go
@@ -0,0 +1,129 @@
+package inject
+
+import (
+	"fmt"
+	"reflect"
+)
+
+// Injector Save the current mapping and parent injectors.
+type Injector struct {
+	parent  *Injector
+	mapping map[reflect.Type]reflect.Value
+}
+
+// NewInjector Create a new injector.
+func NewInjector(parent *Injector) *Injector {
+	return &Injector{
+		parent:  parent,
+		mapping: map[reflect.Type]reflect.Value{},
+	}
+}
+
+// Child returns a sub-injector and inherit the current.
+func (in *Injector) Child() *Injector {
+	return NewInjector(in)
+}
+
+// Map map this value.
+func (in *Injector) Map(val reflect.Value) error {
+	typ := val.Type()
+	in.mapping[typ] = val
+	switch val.Kind() {
+	case reflect.Interface, reflect.Ptr:
+		if val.IsNil() {
+			return fmt.Errorf(`Error: Mapped a null value: %v`, val)
+		}
+		return in.Map(val.Elem())
+	}
+	return nil
+}
+
+// Get Find the value of the type from the mapping and return zero value if not.
+func (in *Injector) Get(typ reflect.Type) reflect.Value {
+	if data, ok := in.Lookup(typ); ok {
+		return data
+	}
+	return reflect.Zero(typ)
+}
+
+// Lookup Find the value of the type from the mapping.
+func (in *Injector) Lookup(typ reflect.Type) (reflect.Value, bool) {
+	if data, ok := in.mapping[typ]; ok {
+		return data, ok
+	}
+	if in.parent != nil {
+		return in.parent.Lookup(typ)
+	}
+	return reflect.Value{}, false
+}
+
+// InjectStruct inject this value, structure will find the `inject` tags for injection.
+func (in *Injector) InjectStruct(val reflect.Value) error {
+	return in.inject(val, true)
+}
+
+// Inject inject this value.
+func (in *Injector) Inject(val reflect.Value) error {
+	return in.inject(val, false)
+}
+
+func (in *Injector) inject(val reflect.Value, structure bool) error {
+	typ := val.Type()
+	if data, ok := in.Lookup(typ); ok && val.CanAddr() {
+		val.Set(data)
+		return nil
+	}
+	switch val.Kind() {
+	case reflect.Ptr:
+		if val.IsNil() {
+			val.Set(reflect.New(typ.Elem()))
+		}
+		return in.inject(val.Elem(), structure)
+	case reflect.Interface:
+		if !val.IsNil() {
+			return in.inject(val.Elem(), structure)
+		}
+	case reflect.Struct:
+		if structure {
+			num := typ.NumField()
+			for i := 0; i != num; i++ {
+				field := typ.Field(i)
+				if _, ok := field.Tag.Lookup("inject"); !ok {
+					continue
+				}
+				in.inject(val.Field(i), structure)
+			}
+			return nil
+		}
+	}
+	return fmt.Errorf(`Error: No values that can be mapped: %v`, val.String())
+}
+
+// Call Find the parameter injection from the mapping and call the function.
+func (in *Injector) Call(fun reflect.Value) ([]reflect.Value, error) {
+	switch fun.Kind() {
+	default:
+		return nil, fmt.Errorf(`Error: Not a function type: %v`, fun.String())
+	case reflect.Interface, reflect.Ptr:
+		if fun.IsNil() {
+			return nil, fmt.Errorf(`Error: Is a null value: %v`, fun.String())
+		}
+		return in.Call(fun.Elem())
+	case reflect.Func:
+		typ := fun.Type()
+		num := typ.NumIn()
+		if num == 0 {
+			return fun.Call(nil), nil
+		}
+
+		args := make([]reflect.Value, 0, num)
+		for i := 0; i != num; i++ {
+			args = append(args, in.Get(typ.In(i)))
+		}
+
+		if typ.IsVariadic() {
+			return fun.CallSlice(args), nil
+		}
+		return fun.Call(args), nil
+	}
+}
---
+++ inject_test.go
@@ -0,0 +1,104 @@
+package inject
+
+import (
+	"reflect"
+	"testing"
+	"time"
+)
+
+var inj = NewInjector(nil)
+
+func TestInject(t *testing.T) {
+	inj := inj.Child()
+
+	type args struct {
+		In  interface{}
+		Out interface{}
+	}
+
+	type I int
+	type T struct {
+		D string
+	}
+	data := []args{
+		{10, new(int)},
+		{"string", new(string)},
+		{I(10), new(I)},
+		{T{"data"}, new(T)},
+		{time.Now(), new(time.Time)},
+	}
+
+	for _, row := range data {
+		in := reflect.ValueOf(row.In)
+		out := reflect.ValueOf(row.Out)
+		err := inj.Map(in)
+		if err != nil {
+			t.Error(err)
+		}
+		err = inj.Inject(out)
+		if err != nil {
+			t.Error(err)
+		}
+		if !reflect.DeepEqual(reflect.Indirect(in).Interface(), reflect.Indirect(out).Interface()) {
+			t.Errorf("Error: Injection failure: %s, %s ", in.String(), out.String())
+		}
+	}
+}
+
+func TestInjectStruct(t *testing.T) {
+	inj := inj.Child()
+
+	type args struct {
+		Ins []interface{}
+		Out interface{}
+	}
+
+	type T struct {
+		S    string    `inject:""`
+		I    *int      `inject:""`
+		Time time.Time `inject:""`
+	}
+	data := []args{
+		{[]interface{}{"A", 2, time.Now()}, new(T)},
+		{[]interface{}{"C", 1, &time.Time{}}, new(T)},
+	}
+
+	for _, row := range data {
+		for _, in := range row.Ins {
+			err := inj.Map(reflect.ValueOf(in))
+			if err != nil {
+				t.Error(err)
+			}
+		}
+		out := reflect.ValueOf(row.Out)
+
+		err := inj.InjectStruct(out)
+		if err != nil {
+			t.Error(err)
+		}
+		out = reflect.Indirect(out)
+		num := out.NumField()
+		for i := 0; i != num; i++ {
+			field := out.Field(i)
+			in := reflect.ValueOf(row.Ins[i])
+			if !reflect.DeepEqual(reflect.Indirect(in).Interface(), reflect.Indirect(field).Interface()) {
+				t.Errorf("Error: Injection failure: %s, %s ", in.String(), field.String())
+			}
+		}
+	}
+}
+
+func TestCall(t *testing.T) {
+	inj := inj.Child()
+	inj.Map(reflect.ValueOf(10))
+	inj.Call(reflect.ValueOf(func(i int, s string) {
+		if i != 10 {
+			t.Fail()
+			return
+		}
+		if s != "" {
+			t.Fail()
+			return
+		}
+	}))
+}
